a348fad2ddaa70180a6547c7d762913486a4cdbe
Rkiefe <kiefe.trabalho@gmail.com>
2025-06-12 16:17:22 +0100

    added a simple meshing example

---
+++ meshExample.jl
@@ -0,0 +1,58 @@
+include("gmsh_wrapper.jl")
+
+function main(meshSize=0,localSize=0,showGmsh=true,saveMesh=false)
+    #=
+        Makes a model with cubes and spheres and refines the mesh on the spheres
+    
+        Input:
+            meshSize  - Mesh size (0 = let gmsh choose)
+            localSize - Size of mesh in every volume beyond the container (0 for no local refinement)
+            saveMesh  - Save mesh to a FEMCE compatible format 
+
+    =#
+    
+    # Create a geometry
+    gmsh.initialize()
+
+    # List of cells inside the container
+    cells = []
+
+    # Import cad file
+    box, bounding_shell_n_surfaces = importCAD("STEP_Models/Fennec_Fox.step",cells)
+
+    # Fragment to make a unified geometry
+    _, fragments = gmsh.model.occ.fragment([(3, box)], cells)
+    gmsh.model.occ.synchronize()
+
+    # Update container volume ID
+    box = fragments[1][1][2]
+
+    # Generate Mesh
+    mesh = Mesh(cells,meshSize,localSize,saveMesh)
+    
+    # Get bounding shell surface id
+    shell_id = gmsh.model.getAdjacencies(3, box)[2]
+
+    # Must remove the surface Id of the interior surfaces
+    shell_id = shell_id[bounding_shell_n_surfaces] # All other, are interior surfaces
+
+    if showGmsh
+        gmsh.fltk.run()
+    end
+    gmsh.finalize()
+
+    println("Number of elements ",size(mesh.t,2))
+    println("Number of Inside elements ",length(mesh.InsideElements))
+    println("Number of nodes ",size(mesh.p,2))
+    println("Number of Inside nodes ",length(mesh.InsideNodes))
+    println("Number of surface elements ",size(mesh.surfaceT,2))
+
+end # end of main
+
+meshSize = 5*60
+localSize = 5
+showGmsh = true
+saveMesh = false
+
+main(meshSize,localSize,showGmsh,saveMesh)
+
