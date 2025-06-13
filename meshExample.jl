a17947e69e882711b2afb93043a6e9f7c9407222
Rkiefe <kiefe.trabalho@gmail.com>
2025-06-13 12:28:50 +0100

    added an example of making your own model with cuboids and spheres

--- meshExample.jl
+++ meshExample.jl
@@ -5,7 +5,59 @@
 
 include("gmsh_wrapper.jl")
 
-function main(meshSize=0,localSize=0,showGmsh=true,saveMesh=false)
+
+function userMade(meshSize=0,localSize=0,showGmsh=true,saveMesh=false)
+    #=
+        Make your own model with cuboids and spheres
+    =#
+    gmsh.initialize()
+
+    # Cuboid dimensions
+    L::Vector{Float64} = [4, 4, 1]
+
+    # >> Model
+    # Create an empty container | Bounding shell
+    box = addSphere([0,0,0],5*maximum(L))
+
+    # Get how many surfaces compose the bounding shell
+    temp = gmsh.model.getEntities(2)            # Get all surfaces of current model
+    bounding_shell_n_surfaces = 1:length(temp)    # Get the number of surfaces in the bounding shell
+
+    # List of cells inside the container
+    cells = []
+
+    # Add the cuboid
+    addCuboid([0,0,0],L,cells,true)
+
+    # Fragment to make a unified geometry
+    _, fragments = gmsh.model.occ.fragment([(3, box)], cells)
+    gmsh.model.occ.synchronize()
+
+    # Update container volume ID
+    box = fragments[1][1][2]
+
+    # Generate Mesh
+    mesh = Mesh(cells,meshSize,localSize,saveMesh)
+    
+    # Get bounding shell surface id
+    shell_id = gmsh.model.getAdjacencies(3, box)[2]
+
+    # Must remove the surface Id of the interior surfaces
+    shell_id = shell_id[bounding_shell_n_surfaces] # All other, are interior surfaces
+
+    if showGmsh
+        gmsh.fltk.run()
+    end
+    gmsh.finalize()
+
+    println("Number of elements ",size(mesh.t,2))
+    println("Number of Inside elements ",length(mesh.InsideElements))
+    println("Number of nodes ",size(mesh.p,2))
+    println("Number of Inside nodes ",length(mesh.InsideNodes))
+    println("Number of surface elements ",size(mesh.surfaceT,2))
+end
+
+function meshCAD(meshSize=0,localSize=0,showGmsh=true,saveMesh=false)
     #=
         Makes a model with cubes and spheres and refines the mesh on the spheres
     
@@ -53,10 +105,11 @@ function main(meshSize=0,localSize=0,showGmsh=true,saveMesh=false)
 
 end # end of main
 
-meshSize = 5*60
-localSize = 5
+meshSize = 20
+localSize = 0.1
 showGmsh = true
 saveMesh = false
 
-main(meshSize,localSize,showGmsh,saveMesh)
+userMade(meshSize,localSize,showGmsh,saveMesh)
+meshCAD(0,0,true,false)
 
