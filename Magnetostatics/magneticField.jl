0bf736c2654b2cbd97d239abb1ab167b40dff7fc
Rkiefe <kiefe.trabalho@gmail.com>
2025-07-16 17:53:55 +0100

    Changed how the geometry is created and how the surface id of the bounding shell is obtained. This was done to manage internal geometry fragmentation better

--- Magnetostatics/magneticField.jl
+++ Magnetostatics/magneticField.jl
@@ -46,36 +46,30 @@ function main(meshSize=0,localSize=0,showGmsh=true,saveMesh=false)
     gmsh.initialize()
 
     # >> Model
-    # Create an empty container
-    # box = addCuboid([0,0,0],5*maximum(L)*[1,1,1])
-    box = addSphere([0,0,0],5*maximum(L))
-
-    # Get how many surfaces compose the bounding shell
-    temp = gmsh.model.getEntities(2)            # Get all surfaces of current model
-    bounding_shell_n_surfaces = 1:length(temp)    # Get the number of surfaces in the bounding shell
-
     # List of cells inside the container
     cells = []
+    addCuboid([0,0,0], L, cells, true)
 
-    # addSphere([0,0,0],0.5,cells)
-    addCuboid([0,0,0],L,cells,true)
+    # Create a bounding shell
+    box = addSphere([0,0,0], 5*maximum(L)) # *[1,1,1]
 
     # Fragment to make a unified geometry
-    _, fragments = gmsh.model.occ.fragment([(3, box)], cells)
+    gmsh.model.occ.fragment([(3,box)], cells)
     gmsh.model.occ.synchronize()
 
-    # Update container volume ID
-    box = fragments[1][1][2]
+    # Get bounding shell surface id
+    shell_id = gmsh.model.getBoundary([(3, box)], false, false, false)
+    shell_id = [s[2] for s in shell_id]
+
+    # Volume surface ids
+    internal_surfaces = gmsh.model.getBoundary(cells, false, false, false)
+    internal_surfaces = [s[2] for s in internal_surfaces]
+
+    shell_id = setdiff(shell_id, internal_surfaces)
 
     # Generate Mesh
     mesh = Mesh(cells,meshSize,localSize,saveMesh)
-    
-    # Get bounding shell surface id
-    shell_id = gmsh.model.getAdjacencies(3, box)[2]
 
-    # Must remove the surface Id of the interior surfaces
-    shell_id = shell_id[bounding_shell_n_surfaces] # All other, are interior surfaces
-    
 
     println("Number of elements ",size(mesh.t,2))
     println("Number of Inside elements ",length(mesh.InsideElements))
