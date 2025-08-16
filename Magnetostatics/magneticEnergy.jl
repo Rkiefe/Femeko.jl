ee08e88d141774101154c20cad127e179e869b0d
rkiefe <kiefe.trabalho@gmail.com>
2025-08-16 22:04:03 +0100

    started a magnetic energy file. Work in progress

---
+++ Magnetostatics/magneticEnergy.jl
@@ -0,0 +1,161 @@
+#=
+    A magnetic field simulation example in 2D
+=#
+
+include("../src/gmsh_wrapper.jl")
+include("../src/FEM.jl")
+include("../src/magneticProperties.jl")
+
+using GLMakie
+
+function main(meshSize=0.0, localSize=0.0, showGmsh=false)
+
+    # vacuum magnetic permeability
+    mu0 = pi*4e-7
+
+    # Temperature
+    T::Float64 = 293.0
+
+    # Applied field
+    Hext::Vector{Float64} = 1.2.*[1,0,0]./mu0    # A/m
+
+    # Convergence criteria
+    picardDeviation::Float64 = 1e-4
+    maxDeviation::Float64 = 1e-6
+    maxAtt::Int32 = 10
+    relax::Float64 = 1.0 # Relaxation factor for N-R ]0, 1.0]
+
+    # Data of magnetic materials
+    materialProperties = Dict("Gd" => DATA())
+    
+    # Load Gd
+    loadMaterial( materialProperties,
+                       "Materials", # Folder with materials
+                       "Gd_MFT",    # Data folder of target material
+                       "Gd",        # Material name
+                       7.9,
+                       T)
+
+
+    # Create model
+    gmsh.initialize()
+
+    cells = []
+
+    # Add a 2D rectangle
+    id = addRectangle([0,0,0], [1, 1], cells)
+    # id = addDisk([0,0,0], 1, cells)
+
+    # Add a container
+    # box = addRectangle([0,0,0], [2, 4])
+    box = addDisk([0,0,0], 4)
+
+    # Combine the geometries
+    gmsh.model.occ.fragment(vcat(cells,[(2,box)]), [])
+    gmsh.model.occ.synchronize()
+
+    # Generate mesh
+    mesh::MESH = Mesh2D(cells, meshSize, localSize)
+
+    println("\nNumber of elements ",size(mesh.t,2))
+    println("Number of Inside elements ",length(mesh.InsideElements))
+    println("Number of nodes ",size(mesh.p,2))
+    println("Number of Inside nodes ",length(mesh.InsideNodes))
+    println("Number of surface elements ",size(mesh.surfaceT,2))
+
+    # Run Gmsh GUI
+    if showGmsh
+       gmsh.fltk.run()
+    end
+    gmsh.fltk.finalize()
+
+
+    # Element centroids
+    centroids::Matrix{Float64} = zeros(2,mesh.nt)
+    for k in 1:mesh.nt
+        nds = mesh.t[:,k]
+        centroids[1,k] = sum(mesh.p[1,nds])/3
+        centroids[2,k] = sum(mesh.p[2,nds])/3
+    end
+
+    # Magnetostatic simulation
+    
+    # Lagrange multiplier technique
+    Lag::Vector{Float64} = zeros(mesh.nv)
+    for k = 1:mesh.nt
+        nds = @view mesh.t[:,k]
+        Lag[nds] .+= mesh.VE[k]/3
+    end
+
+    # Boundary conditions
+    Hext::Vector{Float64} = [1,0]
+    bc::Vector{Float64} = zeros(mesh.ne)
+    for e in 1:mesh.ne
+        if mesh.surfaceT[3,e] == 1 # Only the container boundary
+            bc[e] = dot(Hext, mesh.normal[:,e])
+        end
+    end
+
+    # Magnetic permeability
+    mu::Vector{Float64} = ones(mesh.nt)
+    mu[mesh.InsideElements] .= 2
+
+    # Global stiffness matrix
+    A = stiffnessMatrix2D(mesh, mu)
+
+    # Boundary integral
+    RHS::Vector{Float64} = zeros(mesh.nv)
+    for e in 1:mesh.ne
+        nds = @view mesh.surfaceT[:, e]
+
+        # Length of the edge
+        l::Float64 = norm(mesh.p[:,nds[2]] - mesh.p[:,nds[1]])
+
+        RHS[nds[1]] = RHS[nds[1]] + 0.5*l*bc[e]
+        RHS[nds[2]] = RHS[nds[2]] + 0.5*l*bc[e]
+    end
+
+    # Magnetic scalar potential
+    u::Vector{Float64} = [A Lag;Lag' 0]\[-RHS;0]
+    u = u[1:mesh.nv]
+
+    # Magnetic field
+    H_vectorField::Matrix{Float64} = zeros(mesh.nt,2)
+    for k in 1:mesh.nt
+        nds = @view mesh.t[:,k];
+
+        # Sum the contributions
+        for nd in nds
+            # obtain the element parameters
+            _, bi, ci = abc(mesh.p, nds, nd)
+
+            H_vectorField[k,1] -= u[nd]*bi;
+            H_vectorField[k,2] -= u[nd]*ci;
+        end
+    end
+
+    # Magnetic field intensity
+    H::Vector{Float64} = zeros(mesh.nt)
+    for k in 1:mesh.nt
+        H[k] = norm(H_vectorField[k,:])
+    end
+
+    # Plot result | Uncomment "using GLMakie"
+    fig = Figure()
+    ax = Axis(fig[1, 1], aspect = DataAspect(), title="Magnetic field H")
+
+    scatterPlot = scatter!(ax, 
+        centroids[1,mesh.InsideElements],
+        centroids[2,mesh.InsideElements],
+        color = H[mesh.InsideElements], 
+        colormap=:rainbow, 
+        markersize=20) # 20 .* mesh.VE[mesh.InsideElements]./maximum(mesh.VE[mesh.InsideElements])
+
+    Colorbar(fig[1, 2], scatterPlot, label="H field strength") # Add a colorbar
+    
+    # Display the figure (this will open an interactive window)
+    wait(display(fig)) # This is required only if runing outside the repl
+
+end
+
+main(1.0, 0.05, true)
