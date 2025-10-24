8d5c88a519b3260707faee15f1ab89210383e4f5
Rkiefe <kiefe.trabalho@gmail.com>
2025-10-24 11:57:01 +0100

    a more in depth example of how to make a 2D model, how to cut holes and merge cells to make a unified mesh

--- mesh2Dexample.jl
+++ mesh2Dexample.jl
@@ -1,6 +1,17 @@
 #=
     An example on how to create a 2D model and generate a mesh
     with local refinement
+
+    The model is a square with two holes inside and a bounding shell
+    around the square
+
+    Note: You set the holes last. 
+    	. First add the square
+    	. Then the bounding shell
+    	. Unify the geometry so that the mesh process sees both geometries as 'one model'
+		. Then add the holes
+	Note: The holes only see the 'cells', this way you can't occCut the 'box'
+
 =#
 
 include("src/gmsh_wrapper.jl")
@@ -10,20 +21,29 @@ function main(meshSize=0.0, localSize=0.0, showGmsh=false)
 	gmsh.initialize()
 
 	cells = []
+	box = -1
 
 	# Add a 2D rectangle
-	id = addRectangle([0,0,0], [2, 2], cells)
+	id_rec = addRectangle([0,0,0], [4, 4], cells)
 
-	# Add a container
-	# box = addRectangle([0,0,0], [2, 4])
-    box = addDisk([0,0,0], 4)
+	# Add a bounding shell
+	box = addDisk([0,0,0], 7.0)
+	shell_id = unifyModel(cells, box)
 
-	# Combine the geometries
-	gmsh.model.occ.fragment(vcat(cells,[(2,box)]), [])
-	gmsh.model.occ.synchronize()
+	# Add a hole
+    id = addDisk([-0.75,0,0], 0.2)
+	cells = occCut(cells, [2, id])
+
+    # Add another hole
+    id = addDisk([0.75,0,0], 0.2)
+    cells = occCut(cells, [2, id])
 
     # Generate mesh
 	mesh::MESH = Mesh2D(cells, meshSize, localSize)
+
+	println("\nBox cell ID: ", box)
+	println("Inner cells: ", cells)
+	println("Outer boundary curve ID: ", shell_id, "\n")
 
 	# Run Gmsh GUI
     if showGmsh
@@ -33,4 +53,8 @@ function main(meshSize=0.0, localSize=0.0, showGmsh=false)
 
 end
 
-main(4, 0.1, true)
+meshSize = 2.0
+localSize = 0.1
+showGmsh = true
+
+main(meshSize, localSize, showGmsh)
