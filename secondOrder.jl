0d1dd40cee45208d2f34873c145c5c04c512e232
Rkiefe <kiefe.trabalho@gmail.com>
2025-10-29 15:09:48 +0000

    updated the example to use the new Mesh() function

--- secondOrder.jl
+++ secondOrder.jl
@@ -8,35 +8,27 @@
 include("src/gmsh_wrapper.jl")
 using GLMakie
 
-function SecondOrder(showGmsh=true)
+function SecondOrder(meshSize = 0.0, showGmsh=true)
     #=
         Make your own model with cuboids and spheres
     =#
     gmsh.initialize()
 
-    # Cuboid dimensions
-    L::Vector{Float64} = [1.0, 1.0, 1.0]
+    # Add a cube
+    cell = addCuboid([0,0,0], [1.0, 1.0, 1.0])
 
-    cell = addCuboid([0,0,0], L)
+    # Generate mesh
+    mesh = Mesh([], meshSize, 0.0, false, 2)
 
-    # Mesh
-    # gmsh.option.setNumber("Mesh.MeshSizeMax", 20.0)
-    gmsh.option.setNumber("Mesh.MeshSizeMin", 15.0)
+    # Edges (mid-points)
+    edges = unique(mesh.t[5:10, :])
+    ne = length(edges)
 
-    gmsh.option.setNumber("Mesh.ElementOrder", 2) # Set to quadratic
-    gmsh.model.mesh.generate(3)
-
-    mesh = MESH()
-
-    # Node connectivity
-    t_tags, t = gmsh.model.mesh.getElementsByType(11) # 11 for 2nd order tetrahedral ; 4 for linear
-    mesh.t = reshape(t, 10, Int(size(t,1)/10))
-    mesh.nt = size(mesh.t, 2)
-
-    # Node coordinates
-    _,p,_ = gmsh.model.mesh.getNodes()
-    mesh.p = reshape(p, 3, Int(size(p,1)/3))
-    mesh.nv = size(mesh.p, 2)
+    println("\nNumber of elements: ", mesh.nt)
+    println("Number of vertices: ", mesh.nv)
+    println("Number of surface elements ", size(mesh.surfaceT, 2))
+    println("Number of edges: ", ne)
+    println("")
 
     if showGmsh
         gmsh.fltk.run()
@@ -45,14 +37,18 @@ function SecondOrder(showGmsh=true)
 
     # Plot a single volume element
     nds = mesh.t[:,1]
+
     fig = Figure()
     ax = Axis3(fig[1, 1], aspect = :data)
+
+    # All nodes of the element
     scatter!(ax, mesh.p[1,nds], mesh.p[2,nds], mesh.p[3,nds])
     
-    # The first 4 nodes are the nodes of the linear mesh
+    # Add the first 4 nodes | the nodes of the linear mesh
     scatter!(ax, mesh.p[1,nds[1:4]], mesh.p[2,nds[1:4]], mesh.p[3,nds[1:4]])
     wait(display(fig))
+
 end
 
-SecondOrder(false)
+SecondOrder(0.0, true)
 
