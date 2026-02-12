8eed88f481d5d0b848743837330789b4d76db09f
Rkiefe <kiefe.trabalho@gmail.com>
2026-02-12 18:26:56 +0000

    updated the example to show how I would map the data manually

--- Femeko.cpp/test.cpp
+++ Femeko.cpp/test.cpp
@@ -7,7 +7,19 @@ int main(int argc, char const *argv[])
 	std::vector<double> array = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
 	Eigen::MatrixXd p = Eigen::Map<Eigen::MatrixXd> (array.data(), 3, 2);
 
-	std::cout << p; // correct format
+	std::cout << p  << std::endl; // correct format
+
+	// You could do it manually by
+	Eigen::MatrixXd pManual = Eigen::MatrixXd::Zero(3, 2);
+	int n = 0;
+	for(int nd = 0; nd<2; nd++){ // loop over the nodes
+		for(int i = 0; i<3; i++){ // Loop over x, y, z
+			pManual(i, nd) = array[n];
+			n++;
+		}
+	}
+
+	std::cout << pManual << std::endl; // Also correct format
 
 	return 0;
 }
