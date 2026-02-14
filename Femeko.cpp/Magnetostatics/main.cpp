5e0c3ebfc206cf0ce40cbb3bd79c5f8a120f11f9
Rkiefe <kiefe.trabalho@gmail.com>
2026-02-14 15:10:32 +0000

    finished the magnetostatic field calculation in 2D

--- Femeko.cpp/Magnetostatics/main.cpp
+++ Femeko.cpp/Magnetostatics/main.cpp
@@ -8,14 +8,23 @@
 */
 
 #include "../src/femeko.h"
+#include <fstream> // Save results to .txt
 
 int main()
 {
 	gmsh::initialize();
 
+	// Mesh settings
 	double meshSize = 1.0; 	// Maximum mesh size
 	double localSize = 0.1; // Local element size
 	bool showGmsh = false; // Open gmsh GUI ?
+
+	// Applied field
+	double mu0 = pi*4e-7; // Vacuum magnetic permeability
+	Eigen::Vector2d Bext = {1.0, 0.0}; // Not used in this simulation
+
+	// Linear solver
+	Eigen::ConjugateGradient<Eigen::SparseMatrix<double>> solver;
 
 	// Hold the label of each cell added
 	std::vector<std::pair<int, int>> cells;
@@ -57,9 +66,27 @@ int main()
 	if(showGmsh){ gmsh::fltk::run(); }
 	gmsh::finalize();
 
+	// Centroids of each element
+	Eigen::MatrixXd centers(2, mesh.nt);
+	for(int k = 0; k<mesh.nt; k++){
+		centers.col(k) << 0.0, 0.0;
+		for(int i = 0; i<3; i++){
+			int nd = mesh.t(i, k);
+			centers(0, k) += mesh.p(0, nd)/3.0;
+			centers(1, k) += mesh.p(1, nd)/3.0;
+		} // Average position of the nodes of the element
+	}
+
 	// Magnetic permeability
 	std::vector<double> mu(mesh.nt, 1.0);
 
+	// Magnetization field
+	Eigen::MatrixXd M = Eigen::MatrixXd::Zero(2, mesh.nt);
+	for(int k : mesh.InsideElements){
+		M(0, k) = 1.0;
+	}
+
+	print("\nBuilding the P1 coefficients for each node of each element... ");	
 	// Get the linear basis function for each node of each element
 	Eigen::MatrixXd P1a(3, mesh.nt);
 	Eigen::MatrixXd P1b(3, mesh.nt);
@@ -74,15 +101,66 @@ int main()
 			P1c(i, k) = abc(2);
 		}
 	}
+	println("Done.");
 
+	print("Building the element-wise stiffness matrix... ");
 	// Local stiffness matrix (dense)
 	Eigen::MatrixXd Ak = localStiffnessMatrix2D(mesh, P1b, P1c);
+	println("Done.");
 
 	// Global stiffness matrix (sparse)
 	Eigen::SparseMatrix<double> A = stiffnessMatrix2D(mesh, Ak, mu);
 
+	// Build the load vector
+	Eigen::VectorXd RHS = Eigen::VectorXd::Zero(mesh.nv);
+	for(int k = 0; k<mesh.nt; k++){
+		for(int i = 0; i<3; i++){
+			int nd = mesh.t(i, k);
+			RHS[nd] += ( M(0, k)*P1b(i, k) + M(1, k)*P1c(i, k) ) * mesh.VE[k];
+		}
+	}
 
+	// Solve for the magnetic scalar potential
+	print("Calculating the magnetostatic potential by the Conjugate Gradient method... ");
+	Eigen::VectorXd u = solver.compute(A) .solve(RHS);
+	println("Done.");
 
+	// Calculate the magnetic field from the scalar potential
+	Eigen::MatrixXd Hd = Eigen::MatrixXd::Zero(2, mesh.nt);
+	for(int k = 0; k<mesh.nt; k++){
+
+		// Sum contributions
+		for(int i = 0; i<3; i++){
+			int nd = mesh.t(i, k);
+
+			Hd(0, k) -= u[nd]*P1b(i, k);
+			Hd(1, k) -= u[nd]*P1c(i, k);
+		}
+	}
+
+	print("Saving to .txt files... ");
+	{ // Magnetization
+		std::ofstream file("M.txt");
+		if (file.is_open()) {
+		    file << M << std::endl;
+		    file.close();
+		}
+	}
+	{ // Mesh centroids
+		std::ofstream file("centers.txt");
+		if (file.is_open()) {
+		    file << centers << std::endl;
+		    file.close();
+		}
+	}
+	{ // Magnetic field
+		std::ofstream file("Hd.txt");
+		if (file.is_open()) {
+		    file << Hd << std::endl;
+		    file.close();
+		}
+	}
+	println("Done.");
 
 	return 0;
 }
